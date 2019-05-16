e42446922c40324eabb74bcdf0505c2ced7284be
이재익 <jake@Jakes-Mac.local>
2019-05-16 15:32:19 +0900

    Loggin the User In Complete

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+import passport from "passport";
 import routes from "../routes";
 import User from "../models/User";
 
@@ -5,7 +6,7 @@ export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
 };
 
-export const postJoin = async (req, res) => {
+export const postJoin = async (req, res, next) => {
   const {
     body: { name, email, password, password2 }
   } = req;
@@ -15,26 +16,27 @@ export const postJoin = async (req, res) => {
   } else {
     // To Do : Register User
     try {
-      const user = await User.create({
+      const user = await User({
         name,
         email
       });
+      console.log(password);
       await User.register(user, password);
+      next();
     } catch (error) {
       console.log(error);
+      res.redirect(routes.home);
     }
-
-    // To Do : Log In user
-    res.redirect(routes.home);
   }
 };
 
 export const getLogin = (req, res) =>
   res.render("login", { pageTitle: "Log In" });
 
-export const postLogin = (req, res) => {
-  res.redirect(routes.home);
-};
+export const postLogin = passport.authenticate("local", {
+  failureRedirect: routes.login,
+  successRedirect: routes.home
+});
 
 export const logout = (req, res) => {
   // To Do: Process Log Out
