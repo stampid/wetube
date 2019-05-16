f45a31e9accf4a1c66826fd7adf67cb545052f05
이재익 <jake@Jakes-Mac.local>
2019-05-16 16:43:41 +0900

    Github Log In part One Complete

--- controllers/userController.js
+++ controllers/userController.js
@@ -38,8 +38,15 @@ export const postLogin = passport.authenticate("local", {
   successRedirect: routes.home
 });
 
+export const githubLoginCallback = (accessToken, refreshToken, profile, cb) => {
+  console.log(accessToken, refreshToken, profile, cb);
+};
+
+export const githubLogin = passport.authenticate("github");
+
 export const logout = (req, res) => {
   // To Do: Process Log Out
+  req.logout();
   res.redirect(routes.home);
 };
 
